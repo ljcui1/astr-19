cf134fae5da2ca806a26e4e981130780328dbae9
ljcui1 <114827981+ljcui1@users.noreply.github.com>
2024-01-09 12:26:56 -0800

    Session 2 prompt for Code Journal 1

---
+++ ses2prompt.py
@@ -0,0 +1,15 @@
+sum_in1 = float(input("Please input a floating point number: "))
+sum_in2 = float(input("Please input another floating point number: "))
+sum = sum_in1 + sum_in2
+print("Sum: " + str(sum))
+print("Data Type: " + str(type(sum)))
+diff_in1 = int(input("Please input an integer: "))
+diff_in2 = int(input("Please input another integer: "))
+diff = diff_in1 - diff_in2
+print("Difference: " + str(diff))
+print("Data Type: " + str(type(diff)))
+prod_in1 = float(input("Please input a floating point number: "))
+prod_in2 = int(input("Please input an integer: "))
+prod = prod_in1 * prod_in2
+print("Product: " + str(prod))
+print("Data Type: " + str(type(prod)))

21c139dbfa56adfe798cc0c16578144937d6e841
ljcui1 <114827981+ljcui1@users.noreply.github.com>
2024-01-09 12:44:53 -0800

    Session 3 prompt Code Journal 1

---
+++ ses3prompt.py
@@ -0,0 +1,15 @@
+#This program defines a function f(x) that will return x**3 + 8.
+#The main function will call f(x) with x = 9 and print the result. If the result is greater than 27, then the program will print "YAY!".
+
+def f(x):
+    return (x ** 3 + 8)
+
+def main():
+    x = 9
+    y = f(x)
+    print(str(y))
+    if y > 27:
+        print("YAY!")
+
+if __name__ == "__main__":
+    main()

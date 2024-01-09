59aff4ad59139d9f5fa2b5f36ba2405b2dbca972
ljcui1 <114827981+ljcui1@users.noreply.github.com>
2024-01-09 12:44:29 -0800

    Reorganizing code

--- ses1prompt.py
+++ ses1prompt.py
@@ -1,1 +1,10 @@
-print("Name: Lia Cui\nPronouns: She/they\nMy favorite movie is Everything Everywhere All At Once.\nMy favorite food is most Asian noodle dishes.")
+#This program prints my name, pronouns and information about my favorite movie and food(s).
+
+def printInfo():
+    print("Name: Lia Cui\nPronouns: She/they\nMy favorite movie is Everything Everywhere All At Once.\nMy favorite food is most Asian noodle dishes.")
+
+def main():
+    printInfo()
+
+if __name__ == "__main__":
+    main()
--- ses2prompt.py
+++ ses2prompt.py
@@ -1,15 +1,24 @@
-sum_in1 = float(input("Please input a floating point number: "))
-sum_in2 = float(input("Please input another floating point number: "))
-sum = sum_in1 + sum_in2
-print("Sum: " + str(sum))
-print("Data Type: " + str(type(sum)))
-diff_in1 = int(input("Please input an integer: "))
-diff_in2 = int(input("Please input another integer: "))
-diff = diff_in1 - diff_in2
-print("Difference: " + str(diff))
-print("Data Type: " + str(type(diff)))
-prod_in1 = float(input("Please input a floating point number: "))
-prod_in2 = int(input("Please input an integer: "))
-prod = prod_in1 * prod_in2
-print("Product: " + str(prod))
-print("Data Type: " + str(type(prod)))
+#This program requests the user to input various floating point and integer numbers and does addition, subtraction and multiplication with the given inputs. The program also prints the results and the respective result data types.
+
+def sesMath():
+    sum_in1 = float(input("Please input a floating point number: "))
+    sum_in2 = float(input("Please input another floating point number: "))
+    sum = sum_in1 + sum_in2
+    print("Sum: " + str(sum))
+    print("Data Type: " + str(type(sum)))
+    diff_in1 = int(input("Please input an integer: "))
+    diff_in2 = int(input("Please input another integer: "))
+    diff = diff_in1 - diff_in2
+    print("Difference: " + str(diff))
+    print("Data Type: " + str(type(diff)))
+    prod_in1 = float(input("Please input a floating point number: "))
+    prod_in2 = int(input("Please input an integer: "))
+    prod = prod_in1 * prod_in2
+    print("Product: " + str(prod))
+    print("Data Type: " + str(type(prod)))
+
+def main():
+    sesMath()
+
+if __name__ == "__main__":
+    main()
